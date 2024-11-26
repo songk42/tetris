50b6d0c70e0916311159616000c4e591437b4060
Song Kim <songk@mit.edu>
2024-11-26 16:28:10 -0500

    clearing full rows

--- board.py
+++ board.py
@@ -14,21 +14,21 @@ class Board:
         self.add_piece(piece.random_piece())
         self.game_over = False
 
-    def reset_grid(self):
+    def reset_grid(self) -> None:
         self.grid = np.array([[" " for _ in range(self.width)] * self.height])
     
-    def add_piece(self, piece):
+    def add_piece(self, piece) -> None:
         self.piece = piece
         self.new_piece = False
         if self.check_collision() == 2:
             self.game_over = True
         self.set_piece()
 
-    def step(self):
+    def step(self) -> None:
         """Move the current piece one step down"""
         self.move("down")
     
-    def rotate(self):
+    def rotate(self) -> None:
         """Rotate the current piece"""
         self.clear_piece()
         self.piece.rotate()
@@ -38,7 +38,7 @@ class Board:
         self.set_piece()
         self.new_piece = self.touching_ground()
 
-    def move(self, direction):
+    def move(self, direction) -> None:
         """Move the current piece in the given direction"""
         opposites = {"left": "right", "right": "left", "down": "up", "up": "down"}
         self.clear_piece()
@@ -49,15 +49,25 @@ class Board:
         self.set_piece()
         self.new_piece = self.touching_ground()
     
-    def drop(self):
+    def drop(self) -> None:
         pass
 
-    def clear_piece(self):
+    def clear_rows(self) -> int:
+        """Clears any full rows and returns the number of rows cleared."""
+        rows_cleared = 0
+        for i in range(self.height):
+            if " " not in self.grid[i]:
+                rows_cleared += 1
+                self.grid = np.delete(self.grid, i, axis=0)
+                self.grid = np.concatenate([np.array([[" " for _ in range(self.width)]]), self.grid], axis=0)
+        return rows_cleared
+
+    def clear_piece(self) -> None:
         """Removes piece from grid."""
         for (i, j) in self.piece.get_coords():
             self.grid[i, j] = " "
     
-    def set_piece(self):
+    def set_piece(self) -> None:
         """Adds piece to grid."""
         for (i, j) in self.piece.get_coords():
             self.grid[i, j] = "X"
@@ -88,7 +98,7 @@ class Board:
                 return True
         return False
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """String representation of the board."""
         output = "-" * (self.width + 2) + "\n"
         for i in range(self.height):
--- game.py
+++ game.py
@@ -14,15 +14,18 @@ class Game:
         self.step_interval = 1  # seconds between each step
         self.stdscr = curses.initscr()
         self.stdscr.nodelay(1)
-        # curses.cbreak()
         self.stdscr.keypad(1)
+        # curses.cbreak()
         # curses.nonl()
         # curses.curs_set(0)
         # curses.noecho()
 
     def step(self):
         self.board.step()
+        # somewhere along here, I think some pieces sink down one more level than they should
+        # maybe it's because of those down-arrow inputs
         if self.board.new_piece:
+            rows_cleared = self.board.clear_rows()
             self.board.add_piece(piece.piece_from_letter(self.next_piece.letter))
             self.set_next_piece()
         if self.board.game_over:
@@ -42,26 +45,24 @@ class Game:
 if __name__ == "__main__":
     g = Game()
     step_time = t.time()
-    print("starting game")
     while True:
-        try:
-            user_input = g.stdscr.getch()
-            if user_input == curses.KEY_RIGHT:
-                g.board.move("right")
-            elif user_input == curses.KEY_LEFT:
-                g.board.move("left")
-            elif user_input == curses.KEY_UP:
-                g.board.rotate()
-            elif user_input == curses.KEY_DOWN:
-                g.board.move("down")
-            elif user_input == 32:  # space bar
-                g.board.drop()
-            elif user_input == 113:  # 'q'
-                curses.endwin()
-                sys.exit(0)
-            if t.time() - step_time > g.step_interval:
-                step_time = t.time()
-                g.step()
-            g.refresh_board()
-        except:
-            continue
+        # key controls
+        user_input = g.stdscr.getch()
+        if user_input == curses.KEY_RIGHT:
+            g.board.move("right")
+        elif user_input == curses.KEY_LEFT:
+            g.board.move("left")
+        elif user_input == curses.KEY_UP:
+            g.board.rotate()
+        elif user_input == curses.KEY_DOWN:
+            g.board.move("down")
+        elif user_input == 32:  # space bar
+            g.board.drop()
+        elif user_input == 113:  # 'q'
+            curses.endwin()
+            sys.exit(0)
+        # move piece one step down
+        if t.time() - step_time > g.step_interval:
+            step_time = t.time()
+            g.step()
+        g.refresh_board()
